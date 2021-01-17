6d49d5cef54831ff8802616b9c8ceb63f4e37b74
Jose Rubem <joserubemn31@gmail.com>
2021-01-17 22:54:25 +0000

    chore: added setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React from 'react'
 import {
   ChakraProvider,
   Box,
-} from '@chakra-ui/react';
+  Image,
+  Flex
+} from '@chakra-ui/react'
+import GlobalStyle from './components/GlobalStyle'
+import Orders from './components/Orders'
+import logo from './images/Logo.svg'
 
 const App = () => {
   return (
     <ChakraProvider>
-      <Box
-        fontFamily="Roboto, sans-serif"
-        bg="#EEE"
-        fontWeight="500"
-        color="0A100D"
+      <GlobalStyle/>
+      <Flex
+        m="4rem auto"
+        p="0 2rem"
+        w="100%"
+        maxWidth="740px"
+        align="center"
+        justify="center"
+        direction="column"
       >
-        Hello Worldd
-      </Box>
+        <Image src={logo} alt="EzOrders"/>
+        <Orders/>
+      </Flex>
     </ChakraProvider>
   )
 }
