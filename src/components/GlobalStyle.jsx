269629b46149edc256d8aa4f23948a26eee0fa8a
Jose Rubem <joserubemn31@gmail.com>
2021-01-17 22:54:53 +0000

    feat: added global styles

---
+++ src/components/GlobalStyle.jsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  body {
+    font-family: "Roboto";
+    background-color: #EEE;
+    color: #0A100B;
+    font-weight: 600;
+  }
+`
