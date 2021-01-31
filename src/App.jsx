91a4e6f6c71530e70c0e3dee5264e588100355d1
Jose Rubem <joserubemn31@gmail.com>
2021-01-31 03:39:15 +0000

    feat: added routes, waiters and kitchen pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
 import React from 'react'
-import {
-  ChakraProvider,
-  Box,
-  Image,
-  Flex
-} from '@chakra-ui/react'
+import { ChakraProvider, Image, Flex } from '@chakra-ui/react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import GlobalStyle from './components/GlobalStyle'
-import Orders from './components/Orders'
+import Home from './pages/home'
+import Waiters from './pages/waiters'
+import Kitchen from './pages/kitchen'
 import logo from './images/Logo.svg'
 
 const App = () => {
   return (
     <ChakraProvider>
-      <GlobalStyle/>
+      <GlobalStyle />
       <Flex
-        m="4rem auto"
-        p="0 2rem"
-        w="100%"
-        maxWidth="740px"
-        align="center"
-        justify="center"
-        direction="column"
-      >
-        <Image src={logo} alt="EzOrders"/>
-        <Orders/>
+        m='4rem auto'
+        p='0 2rem'
+        w='100%'
+        maxWidth='740px'
+        align='center'
+        justify='center'
+        direction='column'>
+        <Image src={logo} alt='EzOrders' />
+        <Router>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/waiters'>
+              <Waiters />
+            </Route>
+            <Route path='/kitchen'>
+              <Kitchen />
+            </Route>
+          </Switch>
+        </Router>
       </Flex>
     </ChakraProvider>
   )
