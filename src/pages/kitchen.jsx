7775fae6da18ba722cae03585f5199aff3eda469
Jose Rubem <joserubemn31@gmail.com>
2021-02-04 19:47:12 +0000

    feat: added link to home

--- src/pages/kitchen.jsx
+++ src/pages/kitchen.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import Orders from '../components/Orders'
+import { Flex, Link, Box } from '@chakra-ui/react'
+import { Link as ReactLink } from 'react-router-dom'
 
 const Kitchen = () => {
-  return <Orders />
+  return (
+    <Flex direction='column'>
+      <Box>
+        <Link as={ReactLink} to={'/'}>
+          Back to Home
+        </Link>
+      </Box>
+      <Orders />
+    </Flex>
+  )
 }
 
 export default Kitchen
