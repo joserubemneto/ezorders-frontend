0fc23c70aff6586c4151a3b3ccd9847b33fe242c
Jose Rubem <joserubemn31@gmail.com>
2021-02-04 19:47:45 +0000

    feat: added link to home

--- src/pages/waiters.jsx
+++ src/pages/waiters.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-import { Flex, Text, Textarea, Input, Heading, Button } from '@chakra-ui/react'
+import {
+  Flex,
+  Text,
+  Textarea,
+  Input,
+  Heading,
+  Button,
+  Link,
+} from '@chakra-ui/react'
+import { Link as ReactLink } from 'react-router-dom'
 
 const Waiters = () => {
   return (
     <Flex direction='column' justify='center' align='center' mt='3rem'>
+      <Link as={ReactLink} to='/'>
+        Back to home
+      </Link>
       <Heading fontSize='1.5rem'>New Order</Heading>
       <Flex direction='column' mt='2rem' justify='center'>
         <Text fontSize='1.5rem'>Table number</Text>
