e65faa6dd22458f8370ecadde108497178846d02
Jose Rubem <joserubemn31@gmail.com>
2021-02-07 14:15:08 +0000

    feat: styles improvements

--- src/pages/waiters.jsx
+++ src/pages/waiters.jsx
@@ -9,20 +9,31 @@ import {
   Link,
 } from '@chakra-ui/react'
 import { Link as ReactLink } from 'react-router-dom'
+import { ReactComponent as Back } from '../assets/back.svg'
 
 const Waiters = () => {
   return (
-    <Flex direction='column' justify='center' align='center' mt='3rem'>
-      <Link as={ReactLink} to='/'>
-        Back to home
-      </Link>
-      <Heading fontSize='1.5rem'>New Order</Heading>
+    <Flex direction='column' mt='3rem' maxWidth='565px' w='100%'>
+      <Flex align='center' justify='space-between'>
+        <Heading mr='3rem' fontSize='1.5rem'>
+          Waiters
+        </Heading>
+        <Flex align='center' justify='center'>
+          <Text mr='0.5rem'>Back to home</Text>
+          <Link as={ReactLink} to={'/'}>
+            <Back />
+          </Link>
+        </Flex>
+      </Flex>
+      <Heading alignSelf='center' fontSize='1.5rem' mt='2rem'>
+        New Order
+      </Heading>
       <Flex direction='column' mt='2rem' justify='center'>
-        <Text fontSize='1.5rem'>Table number</Text>
+        <Text fontSize='1.2rem'>Table number</Text>
         <Input placeholder='Table number' />
       </Flex>
       <Flex direction='column' mt='2rem' justify='center'>
-        <Text fontSize='1.5rem'>Order</Text>
+        <Text fontSize='1.2rem'>Order</Text>
         <Textarea placeholder='Order details' />
       </Flex>
       <Button
