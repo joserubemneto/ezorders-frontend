98bc625e62a7d1452e76bafe26649340041e02a2
Jose Rubem <joserubemn31@gmail.com>
2021-02-07 14:14:54 +0000

    feat: styles improvements

--- src/pages/kitchen.jsx
+++ src/pages/kitchen.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import Orders from '../components/Orders'
-import { Flex, Link, Box } from '@chakra-ui/react'
+import { Flex, Link, Text, Heading } from '@chakra-ui/react'
+import { CheckIcon } from '@chakra-ui/icons'
 import { Link as ReactLink } from 'react-router-dom'
+import { ReactComponent as Back } from '../assets/back.svg'
 
 const Kitchen = () => {
   return (
-    <Flex direction='column'>
-      <Box>
-        <Link as={ReactLink} to={'/'}>
-          Back to Home
-        </Link>
-      </Box>
+    <Flex direction='column' mt='3rem'>
+      <Flex align='center' justify='space-between'>
+        <Heading mr='3rem' fontSize='1.5rem'>
+          Kitchen
+        </Heading>
+        <Flex align='center' justify='center'>
+          <Text mr='0.5rem'>Back to home</Text>
+          <Link as={ReactLink} to={'/'}>
+            <Back />
+          </Link>
+        </Flex>
+      </Flex>
+      <Flex align='center' justify='center' mt='2rem'>
+        <Text fontSize='1rem' mr='0.5rem'>
+          Clean done orders
+        </Text>
+        <CheckIcon
+          bgColor='#2AC769'
+          borderRadius='100px'
+          p='0.5rem'
+          color='#FFF'
+          w='35px'
+          h='35px'
+        />
+      </Flex>
       <Orders />
     </Flex>
   )
