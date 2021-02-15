dbad02858e95238de7e1a60802acb85bbc8f4850
Jose Rubem <joserubemn31@gmail.com>
2021-02-15 02:51:39 +0000

    refactor: removed clean done orders button

--- src/pages/kitchen.jsx
+++ src/pages/kitchen.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Orders from '../components/Orders'
-import { Flex, Link, Text, Heading, Button } from '@chakra-ui/react'
-import { CheckIcon } from '@chakra-ui/icons'
+import { Flex, Link, Text, Heading } from '@chakra-ui/react'
 import { Link as ReactLink } from 'react-router-dom'
 import { ReactComponent as Back } from '../assets/back.svg'
 
@@ -18,29 +17,6 @@ const Kitchen = () => {
             <Back />
           </Link>
         </Flex>
-      </Flex>
-      <Flex align='center' justify='center' mt='2rem'>
-        <Button
-          fontSize='1rem'
-          position='relative'
-          mr='0.5rem'
-          border='2px solid #0A100D'
-          color='#0A100D'
-          _hover={{ bg: '#2AC769' }}
-          _focus={{ outline: 'none', bg: '#2AC769' }}
-          _before={{
-            content: `''`,
-            position: 'absolute',
-            top: '0%',
-            width: '100%',
-            height: '100%',
-            bgColor: '#0A100D',
-            zIndex: -1,
-            transition: '.7s ease',
-          }}>
-          <Text>Clean done orders</Text>
-          <CheckIcon borderRadius='100px' p='0.5rem' w='35px' h='35px' />
-        </Button>
       </Flex>
       <Orders />
     </Flex>
