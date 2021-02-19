60feaf5b9d0ad96ec4a984d1d01e8007561826b4
Jose Rubem <joserubemn31@gmail.com>
2021-02-19 21:07:15 +0000

    feat: styles improvements

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Flex, Heading, Link } from '@chakra-ui/react'
+import { Flex, Heading, Link, Button } from '@chakra-ui/react'
 import { Link as RouterLink } from 'react-router-dom'
 
 const Home = () => {
@@ -9,12 +9,69 @@ const Home = () => {
       justify='center'
       align='center'
       fontWeight='bold'
-      textDecoration='underline'>
-      <Link as={RouterLink} to='/waiters'>
-        <Heading mr='3rem'>Waiters</Heading>
+      textDecoration='none'>
+      <Link as={RouterLink} to='/waiters' _hover={{ textDecoration: 'none' }}>
+        <Button
+          fontSize='2rem'
+          position='relative'
+          padding='1.5rem 2rem'
+          mr='3rem'
+          bg='none'
+          overflow='hidden'
+          border='2px solid #0A100D'
+          color='#0A100D'
+          _focus={{ outline: 'none' }}
+          _hover={{
+            color: '#FFF',
+            border: '2px solid #2AC769',
+            _before: {
+              width: '100%',
+            },
+          }}
+          _before={{
+            content: `''`,
+            position: 'absolute',
+            top: '0%',
+            width: '0',
+            height: '100%',
+            bgColor: '#2AC769',
+            zIndex: -1,
+            transition: '.5s ease',
+          }}
+          _active={{ bg: '#2AC769' }}>
+          Waiters
+        </Button>
       </Link>
-      <Link as={RouterLink} to='/kitchen'>
-        <Heading>Kitchen</Heading>
+      <Link as={RouterLink} to='/kitchen' _hover={{ textDecoration: 'none' }}>
+        <Button
+          fontSize='2rem'
+          position='relative'
+          padding='1.5rem 2rem'
+          bg='none'
+          overflow='hidden'
+          border='2px solid #0A100D'
+          color='#0A100D'
+          _focus={{ outline: 'none' }}
+          _hover={{
+            color: '#FFF',
+            border: '2px solid #2AC769',
+            _before: {
+              width: '100%',
+            },
+          }}
+          _before={{
+            content: `''`,
+            position: 'absolute',
+            top: '0%',
+            width: '0',
+            height: '100%',
+            bgColor: '#2AC769',
+            zIndex: -1,
+            transition: '.5s ease',
+          }}
+          _active={{ bg: '#2AC769' }}>
+          Kitchen
+        </Button>
       </Link>
     </Flex>
   )
